00ef5018c0b8b33ca0362d0ca4d2ce829ff70fd2
Douglas Linsmeyer <douglinsmeyer@gmail.com>
2019-06-24 20:39:57 -0700

    Adds permission mgmt to user controller.

--- webservice/src/controllers/UserController.js
+++ webservice/src/controllers/UserController.js
@@ -1,11 +1,17 @@
 import UserModel from '../models/user';
+import permitter from '../services/security/permitter';
 
 let controller = {};
+let userPermitter = permitter.getPermitter('user');
 
 controller.update = async (req, res) => {
     try {
         let user = await UserModel.findByIdAndUpdate(req.body.user.id, req.body.user, { new: true, });
         
+        if (!userPermitter.canUpdate(user, req.user)) {
+            res.status(401).send('Unauthorized.');
+        }
+
         if (!user) {
             res.status(400).send({ error: "Update failed.", });
         }
